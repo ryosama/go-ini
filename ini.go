2cec58630edf954e847fc5b270e7f1f5fe7a9262
ryosama <benjamin.poulain@gmail.com>
2018-07-26 11:54:31 +0200

    Add internal management of commentaries Better tests

--- ini.go
+++ ini.go
@@ -1,13 +1,35 @@
-// This package permits interaction with ini files (configuration file format for windows)
-// You can parse, read values, set values and save your ini files
+/*
+This package permits interaction with ini files (configuration file format for windows).
+You can parse, read values, set values and save your ini files
+
+Basic Usage :
+
+myIni := new(ini.Ini)
+
+if err := myIni.LoadFromFile("config.ini") ; err {
+	panic("Unable to load configuration : ", err)
+}
+
+myHost := myIni.Get("Server","host")
+
+if myIni.Exists("Server","port") { // test if key exists
+	myPort := int( myIni.Get("Server","port") )
+} else {
+	myPort := 80
+}
+
+myIni.Set("Server","host","127.0.0.1")
+myIni.Save()
+*/
 package ini
 
 import (
 	"io/ioutil"
 	"strings"
 	"regexp"
-	"os"
 	"errors"
+	"os"
+	_ "github.com/davecgh/go-spew/spew"
 )
 
 
@@ -16,17 +38,35 @@ Create a new Ini object
 
 Example :
 
-myini := new(ini.Ini)
+myIni := new(ini.Ini)
 */
 type Ini struct {
-	filename string
-	sections map[string]map[string] string
+	data 	 	map[string]Section
 
-	// default "" : Add this string before every items
-	ItemPrefix string
+	// Last filename pass to Load or Save
+	Filename 	string
 
-	// default "" : Add this string before every new sections (except the first one)
+	// Add this string before every items (default is "")
+	ItemPrefix 	string
+
+	// Add this string before every new sections (except the first one) (default is "")
 	SectionSeparator string
+
+	// If true Remove the comments while saving (default is false)
+	NoComments bool
+}
+
+
+// A section has items and comments
+type Section struct {
+	items 	 map[string]Item
+	comments []string
+}
+
+// An item has value and comments
+type Item struct {
+	value 	   string
+	comments []string
 }
 
 /*
@@ -34,16 +74,16 @@ filename : Read ini format from this file
 
 Example :
 
-err := myini.LoadFromFile("config.ini")
+err := myIni.LoadFromFile("config.ini")
 */
 func (this *Ini) LoadFromFile(filename string) error {
-	this.filename = filename
-	content, err := ioutil.ReadFile(this.filename)
+	this.Filename = filename
+	content, err := ioutil.ReadFile(this.Filename)
 	if err != nil {
        return err
     }
-    t := string(content)
-    this.LoadFromString(&t)
+    tmp := string(content)
+    this.LoadFromString(&tmp)
     return nil
 }
 
@@ -52,39 +92,248 @@ Load data from a string pointer
 
 Example :
 
-content := "[section1] \n item1=value1"
+content := `
+
+[section1]
+
+item1=value1`
 
 myini.LoadFromString( &content )
 */
 func (this *Ini) LoadFromString(content *string) {
 
 	current_section := ""
-    content_array := strings.Split( *content, "\n")
+	comments 	    := make([]string,0)
 
-    re_comment 	:= regexp.MustCompile("\\s*;")
+    content_array := strings.Split( *content, "\n") // split lines into array
+
+    re_comment 	:= regexp.MustCompile("\\s*[;#](.*)")
     re_section 	:= regexp.MustCompile("\\s*\\[\\s*(.+?)\\s*\\]")
 	re_item 	:= regexp.MustCompile("\\s*(.+?)\\s*=\\s*(.+)\\s*")
 
-   	this.sections = make(map[string]map[string]string)
+   	this.data = make(map[string]Section)
 
-    for _, value := range content_array {
+    for _, value := range content_array { // for each line
 
-    	if re_comment.MatchString( value ) { // un commentaire
-    		
-		} else if 	matches := re_section.FindStringSubmatch( value ) ; matches != nil { // un section
-			this.sections[matches[1]] = map[string]string{}
-			current_section = matches[1]
+    	if matches := re_comment.FindStringSubmatch( value ) ; matches != nil { // a comment
+    		comments = append(comments, strings.TrimSpace(matches[1]) )
 
-    	} else if 	matches := re_item.FindStringSubmatch( value ) ; matches != nil { // un item
-			this.sections[current_section][matches[1]] = strings.TrimSpace(matches[2])
+		} else if 	matches := re_section.FindStringSubmatch( value ) ; matches != nil { // a section
+			section := strings.TrimSpace(matches[1])
+			current_section = section 		// set active section
+
+			var d Section
+				d.comments = comments
+			this.data[current_section] = d
+			comments = make([]string,0) // clears comments
+			
+    	} else if 	matches := re_item.FindStringSubmatch( value ) ; matches != nil { // an item
+    		name  := strings.TrimSpace(matches[1])
+    		value := strings.TrimSpace(matches[2])
+
+    		var tmp Item
+				tmp.comments = comments
+				tmp.value 	 = value
+
+			s := this.data[current_section]
+			if 	s.items == nil { // create structure for the first time
+				s.items = make(map[string]Item)
+			}
+			s.items[name] = tmp
+			this.data[current_section] = s
+			comments = make([]string,0) // clears comments
+		
     	}
     }
+}
+
+
+/* Returns all the sections of the ini file */
+func (this *Ini) GetSections() []string {
+	sections := make( []string, len(this.data) )
+	i := 0
+	for key,_ := range this.data {
+		sections[i] = key
+		i++
+	}
+	return sections
+}
+
+/* Returns all the items of the ini file for a given section */
+func (this *Ini) GetItems(section string) []string {
+	items := make( []string, len(this.data[section].items) )
+	i := 0
+	for key,_ := range this.data[section].items {
+		items[i] = key
+		i++
+	}
+	return items
+}
+
+/* Returns true or false if the section exists */
+func (this *Ini) SectionExists(section string) bool {
+	_, exists := this.data[section]
+	return exists
+}
+
+
+/* Returns true or false if the item exists for the given section */
+func (this *Ini) ItemExists(section string, item string) bool {
+	_, exists := this.data[section]
+	if exists {
+		_, exists := this.data[section].items[item]
+		return exists
+	}
+	return false
+}
+
+/* Alias for ItemExists */
+func (this *Ini) Exists(section string, item string) bool {
+	return this.ItemExists(section, item)
+}
+
+/*
+Returns the items value of the ini file for a given section and item (and true as second return value)
+
+If the item does not exists, return false as second return value
+
+Example :
+
+value, success := myini.GetItem("section1","item1")
+*/
+func (this *Ini) GetItem(section string, item string) (string, bool) {
+	if this.ItemExists(section, item) {
+		return this.data[section].items[item].value, true
+	}
+	return "", false
+}
+
+/* Alias for GetItem */
+func (this *Ini) Get(section string, item string) (string, bool) {
+	return this.GetItem(section, item)
+}
+
+/*
+Set the value of an item
+
+Returns true if success, false section or item does not exists
+*/
+func (this *Ini) SetItem(section string, item string, value string) bool {
+	if this.ItemExists(section,item) {
+		var i Item = this.data[section].items[item]
+		i.value = value
+		this.data[section].items[item] = i
+		return true
+	}
+	return false
+}
+
+/* Alias for SetItem */
+func (this *Ini) Set(section string, item string, value string) bool {
+	return this.SetItem(section, item, value)
+}
+
+/*
+Rename a section
+
+Returns true if success, false if section does not exists
+*/
+func (this *Ini) RenameSection(oldName string, newName string) bool {
+	if this.SectionExists(oldName) {
+		this.data[newName] = this.data[oldName]
+		delete(this.data, oldName)
+		return true
+	}
+	return false
+}
+
+/*
+Rename an item
+
+Returns true if success, false if section or item does not exists
+*/
+func (this *Ini) RenameItem(section, oldName string, newName string) bool {
+	if this.Exists(section, oldName) {
+		this.data[section].items[newName] = this.data[section].items[oldName]
+		delete(this.data[section].items, oldName)
+		return true
+	}
+	return false
+}
+
+/*
+Add a section
+
+Returns true if success, false if section already exists
+*/
+func (this *Ini) AddSection(section string) bool {
+	if this.SectionExists(section) {
+		return false
+	}
+	var s Section
+	s.items 	= make(map[string]Item)
+	s.comments 	= make([]string,0)
+	this.data[section] = s
+	return true
+}
+
+/* 
+Add an item
+
+Returns true if success, false if item already exists
+*/
+func (this *Ini) AddItem(section string, item string, value string) bool {
+	if this.SectionExists(section) {
+		if this.ItemExists(section,item) {
+			return false
+		}
+
+		var tmp Item
+			tmp.value = value
+			tmp.comments = make([]string,0)
+		this.data[section].items[item] = tmp
+		return true
+
+	} else {
+		// section does not exist --> create it
+		this.AddSection(section)
+		this.AddItem(section, item, value)
+		return true
+	}
+}
+
+// Set a value for an item, create section and item if needed
+func (this *Ini) SetOrCreate(section string, item string, value string) {
+	this.AddItem(section,item,value)
+	this.Set(section,item,value)
+}
+
+/*
+Save the ini format to a file
+
+Example :
+
+err := myIni.Save() // use myIni.Filename to save
+
+err := myIni.Save("new_config.ini") // use new_config.ini and set myIni.Filename
+*/
+func (this *Ini) Save(params ...string) error {
+	if len(params)>0 {
+		this.Filename = params[0]
+	}
+
+	if this.Filename == "" {
+		return errors.New("You must specify a filename before saving")
+	}
+
+	return ioutil.WriteFile(this.Filename,  []byte( this.Sprint() ), os.ModePerm)
 }
 
 /*
 Return the ini format into a formatted string
 
-TIPS : You can set myIniObj.ItemPrefix and myIniObj.SectionSeparator to tweak format aspect
+TIPS :
+You can set myIni.ItemPrefix, myIni.SectionSeparator and myIni.NoComments to tweak format aspect
 */
 func (this *Ini) Sprint() string {
 	s := ""
@@ -108,196 +357,18 @@ func (this *Ini) Sprint() string {
 /*
 Print the ini format into a formatted string
 
-TIPS : You can set myIniObj.ItemPrefix and myIniObj.SectionSeparator to tweak format aspect
+TIPS :
+You can set myIni.ItemPrefix, myIni.SectionSeparator and myIni.NoComments to tweak format aspect
 */
 func (this *Ini) Print() {
 	print(this.Sprint())
 }
 
-/* Returns all the sections of the ini file */
-func (this *Ini) GetSections() []string {
-	sections := make( []string, len(this.sections) )
-	i := 0
-	for key,_ := range this.sections {
-		sections[i] = key
-		i++
-	}
-	return sections
-}
-
-/* Returns all the items of the ini file for a given section */
-func (this *Ini) GetItems(section string) []string {
-	items := make( []string, len(this.sections[section]) )
-	i := 0
-	for key,_ := range this.sections[section] {
-		items[i] = key
-		i++
-	}
-	return items
-}
-
-/*
-Returns the items value of the ini file for a given section and item (and true as second return value)
-
-If the item does not exists, return false as second return value
-
-Example :
-
-value, success := myini.GetItem("section1","item1")
+/* TO DO
+GetSectionComments(section string) []strings
+GetItemComments(section string,item string) []strings
+DeleteSection(section string) bool
+DeleteItem(section string,item string) bool
+DeleteComment(id int) bool
+DeleteComments() bool
 */
-func (this *Ini) GetItem(section string, item string) (string, bool) {
-	if this.ItemExists(section, item) {
-		return this.sections[section][item], true
-	} else {
-		return "", false
-	}
-}
-
-/* Alias for GetItem */
-func (this *Ini) Get(section string, item string) (string, bool) {
-	return this.GetItem(section, item)
-}
-
-/* Returns true or false if the section exists */
-func (this *Ini) SectionExists(section string) bool {
-	_, exists := this.sections[section]
-	return exists
-}
-
-/* Returns true or false if the item exists for the given section */
-func (this *Ini) ItemExists(section string, item string) bool {
-	_, exists := this.sections[section]
-	if exists {
-		_, exists := this.sections[section][item]
-		return exists
-	} else {
-		return false
-	}
-}
-
-/* Alias for ItemExists */
-func (this *Ini) Exists(section string, item string) bool {
-	return this.ItemExists(section, item)
-}
-
-/*
-Set the value of an item
-
-Returns true if success, false section or item does not exists
-*/
-func (this *Ini) SetItem(section string, item string, value string) bool {
-	if this.ItemExists(section,item) {
-		this.sections[section][item] = value
-		return true
-	} else {
-		return false
-	}
-}
-
-/* Alias for SetItem */
-func (this *Ini) Set(section string, item string, value string) bool {
-	return this.SetItem(section, item, value)
-}
-
-/*
-Rename a section
-
-Returns true if success, false if section does not exists
-*/
-func (this *Ini) RenameSection(oldName string, newName string) bool {
-	newSections := make(map[string]map[string]string)
-	var state bool = false
-	for _, section := range this.GetSections() {
-		if section == oldName { // rename
-			newSections[newName] = this.sections[oldName]
-			state = true
-		} else { // only copy
-			newSections[section] = this.sections[section]
-		}
-	}
-	this.sections = newSections
-	return state
-}
-
-/*
-Rename an item
-
-Returns true if success, false if section or item does not exists
-*/
-func (this *Ini) RenameItem(section, oldName string, newName string) bool {
-	newItems := make(map[string]string)
-	var state bool = false
-	for _, item := range this.GetItems(section) {
-		if item == oldName { // rename
-			newItems[newName] = this.sections[section][oldName]
-			state = true
-		} else { // only copy
-			newItems[item] = this.sections[section][item]
-		}
-	}
-	this.sections[section] = newItems
-	return state
-}
-
-/*
-Add a section
-
-Returns true if success, false if section already exists
-*/
-func (this *Ini) AddSection(section string) bool {
-	if this.SectionExists(section) {
-		return false
-	}
-	this.sections[section] = make(map[string]string)
-	return true
-}
-
-/* 
-Add an item
-
-Returns true if success, false if item already exists
-*/
-func (this *Ini) AddItem(section string, item string, value string) bool {
-	if this.SectionExists(section) {
-		if this.ItemExists(section,item) {
-			return false
-		}
-		this.sections[section][item] = value
-		return true
-
-	} else {
-		// section does not exist --> create it
-		this.AddSection(section)
-		this.AddItem(section, item, value)
-		return true
-	}
-}
-
-// Set a value for an item, create section and item if needed
-func (this *Ini) SetOrCreate(section string, item string, value string) {
-	this.AddItem(section,item,value)
-	this.Set(section,item,value)
-}
-
-/*
-Save() error
-
-Save(filename string) error
-
-Save the ini format to a file
-
-Example :
-
-err := myini.Save("new_config.ini")
-*/
-func (this *Ini) Save(params ...string) error {
-	if len(params)>0 {
-		this.filename = params[0]
-	}
-
-	if this.filename == "" {
-		return errors.New("You must specify a filename before saving")
-	}
-
-	return ioutil.WriteFile(this.filename,  []byte( this.Sprint() ), os.ModePerm)
-}
--- ini_test.go
+++ ini_test.go
@@ -1,131 +1,176 @@
 package ini
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"sort"
+	_ "github.com/davecgh/go-spew/spew"
+)
 
 func Test (t *testing.T) {
-	result 	:= ""
-	success := false
-	myini := new(Ini)
+	//var result string = ""
+	var a []string
+	var s string	= ""
+	var b bool 		= false
+
+	myIni := new(Ini)
 	content := `
+# quelques comment en debut de fichier
+ ; un autre commentaire en debut
 	[section1]
+# ce com est pour section1.item1
+# ce 2eme com est pour section1.item1
+
 		item1=value1
+
 	# comment with a sharp
+
 	item2=value2
 ; comment with a dot-comma
+; second line of comments
  [ section2  ]
 	item1=value1
 	; comment 3
+# second line of comments for comment 3
 	item2=value2
 `
-	myini.LoadFromString(&content)
+	myIni.LoadFromString(&content)
 
 	// GetSections
-	if len(myini.GetSections()) != 2 {
-		t.Error("For", "GetSections()","expected", 2 ,"got", len(myini.GetSections()) )
+	a = myIni.GetSections() ; sort.Strings(a) ;	s = fmt.Sprintf("%v", a )
+	exceptedSections := fmt.Sprintf("%v", []string{"section1","section2"} )
+	if s != exceptedSections {
+		t.Error("For", "GetSections()", "expected",	exceptedSections, "got", s )
 	}
 
 	// GetItems
-	if len(myini.GetItems("section1")) != 2 {
-		t.Error("For", "GetItems(section1)","expected", 2 ,"got", len(myini.GetItems("section1")) )
+	a = myIni.GetItems("section1") ; sort.Strings(a) ;	s = fmt.Sprintf("%v", a )
+	exceptedItems := fmt.Sprintf("%v", []string{"item1","item2"} )
+	if s != exceptedItems {
+		t.Error("For", "GetItems(section1)", "expected", exceptedItems , "got", s )
 	}
 
 	// SectionExists
-	if myini.SectionExists("section1") == false {
-		t.Error("For", "SectionExists(section1)","expected", true ,"got", myini.SectionExists("section1") )
+	if b = myIni.SectionExists("section1") ; !b {
+		t.Error("For", "SectionExists(section1)", "expected", true , "got", b)
 	}
 
 	// SectionExists
-	if myini.SectionExists("does not exists") == true {
-		t.Error("For", "SectionExists(does not exists)","expected", false ,"got", myini.SectionExists("does not exists") )
+	if b = myIni.SectionExists("does not exists") ; b {
+		t.Error("For", "SectionExists(does not exists)", "expected", false , "got", b)
 	}
 
 	// Exists
-	if myini.Exists("section1","item1") == false {
-		t.Error("For", "Exists(section1,item1)","expected", true ,"got", myini.Exists("section1","item1") )
+	if b = myIni.Exists("section1","item1") ; !b {
+		t.Error("For", "Exists(section1,item1)","expected", true , "got", b )
 	}
 
 	// Exists
-	if myini.Exists("section1","does not exists") == true {
-		t.Error("For", "Exists(section1,does not exists)","expected", false ,"got", myini.Exists("section1","does not exists") )
+	if b = myIni.Exists("section1","does not exists"); b {
+		t.Error("For", "Exists(section1,does not exists)","expected", false , "got", b )
 	}
 
 	// Exists
-	if myini.Exists("does not exists","does not exists") == true {
-		t.Error("For", "Exists(does not exists,does not exists)","expected", false ,"got", myini.Exists("does not exists","does not exists") )
+	if b = myIni.Exists("does not exists","does not exists") ; b {
+		t.Error("For", "Exists(does not exists,does not exists)","expected", false , "got", b )
 	}
 
 	// GET
-	result, _ 	= myini.Get("section1","item1")
-	if result != "value1" {
-		t.Error("For", "Get(section1,item1)","expected", "value1","got", result)
+	expectedValue := "value1"
+	if s, _ = myIni.Get("section1","item1") ; s != expectedValue {
+		t.Error("For", "Get(section1,item1)","expected", expectedValue, "got", s)
 	}
 
 	// SET
-	success 	= myini.Set("section1","item2","edit value")
-	if !success {
-		t.Error("For", "Set(section1,item2,edit value)","expected", true,"got", false)
+	if b = myIni.Set("section1","item2","edit value") ; !b {
+		t.Error("For", "Set(section1,item2,edit value)","expected", true, "got", b)
 	}
-	// GET again
-	result, _ 	= myini.Get("section1","item2")
-	if result != "edit value" {
-		t.Error("For", "Get(section1,item2)","expected", "edit value", "got", result)
+	// GET again to test if SET is OK
+	expectedValue = "edit value"
+	if s, _ = myIni.Get("section1","item2") ; s != expectedValue {
+		t.Error("For", "Get(section1,item2)","expected", expectedValue, "got", s)
 	}
 
 	// RenameSection
-	success 	= myini.RenameSection("section2","section3")
-	if !success {
-		t.Error("For", "RenameSection(section2,section3)","expected", true, "got", success)
+	if b = myIni.RenameSection("section2","rename section") ; !b {
+		t.Error("For", "RenameSection(section2,rename section)","expected", true, "got", b)
 	}
 	// SectionExists
-	success 	= myini.SectionExists("section3")
-	if !success {
-		t.Error("For", "SectionExists(section3)","expected", true, "got", success)
+	if b = myIni.SectionExists("rename section") ; !b {
+		t.Error("For", "SectionExists(rename section)","expected", true, "got", b)
+	}
+	// SectionExists
+	if b = myIni.SectionExists("section2") ; b {
+		t.Error("For", "SectionExists(section2)","expected", false, "got", b)
 	}
 
 	// RenameItem
-	success 	= myini.RenameItem("section3","item1","edit item")
-	if !success {
-		t.Error("For", "RenameItem(section3,item1,edit item)","expected", true, "got", success)
+	if b = myIni.RenameItem("rename section","item1","renamed item") ; !b {
+		t.Error("For", "RenameItem(rename section,item1,renamed item)","expected", true, "got", b)
 	}
 	// Exists
-	success 	= myini.Exists("section3","edit item")
-	if !success {
-		t.Error("For", "Exists(section3,edit item)","expected", true, "got", success)
+	if b = myIni.Exists("rename section","renamed item") ; !b {
+		t.Error("For", "Exists(rename section,renamed item)","expected", true, "got", b)
+	}
+	// Exists
+	if b = myIni.Exists("rename section","item1") ; b {
+		t.Error("For", "Exists(rename section,item1)","expected", false, "got", b)
 	}
 
 	// AddSection
-	success 	= myini.AddSection("added section")
-	if !success {
-		t.Error("For", "AddSection(added section)","expected", true, "got", success)
+	if b = myIni.AddSection("added section") ; !b {
+		t.Error("For", "AddSection(added section)","expected", true, "got", b)
 	}
 	// AddSection
-	success 	= myini.AddSection("added section")
-	if success != false {
-		t.Error("For", "AddSection(added section)","expected", false, "got", success)
+	if b = myIni.AddSection("added section") ; b {
+		t.Error("For", "AddSection(added section)","expected", false, "got", b)
+	}
+	// GetSections
+	a = myIni.GetSections() ; sort.Strings(a) ;	s = fmt.Sprintf("%v", a )
+	exceptedSections = fmt.Sprintf("%v", []string{"added section", "rename section", "section1"} )
+	if s != exceptedSections {
+		t.Error("For", "GetSections()", "expected",	exceptedSections, "got", s )
 	}
 
 	// AddItem
-	success 	= myini.AddItem("added section", "added item", "add value")
-	if !success {
-		t.Error("For", "AddSection(added section, added item, add value)","expected", true, "got", success)
+	if b = myIni.AddItem("added section", "added item", "added value") ; !b {
+		t.Error("For", "AddItem(added section, added item, added value)","expected", true, "got", b)
 	}
-	// AddItem
-	success 	= myini.AddItem("added section", "added item", "add value")
-	if success != false {
-		t.Error("For", "AddSection(added section, added item, add value)","expected", false, "got", success)
+	if b = myIni.AddItem("added section", "added item", "added value") ; b {
+		t.Error("For", "AddItem(added section, added item, added value)","expected", false, "got", b)
+	}
+	// GetItems
+	a = myIni.GetItems("added section") ; sort.Strings(a) ;	s = fmt.Sprintf("%v", a )
+	exceptedItems = fmt.Sprintf("%v", []string{"added item"} )
+	if s != exceptedItems {
+		t.Error("For", "GetItems(added section)", "expected", exceptedItems , "got", s )
+	}
+	// Get check if value is correct
+	expectedValue = "added value"
+	if s, _ = myIni.Get("added section","added item") ; s != expectedValue {
+		t.Error("For", "Get(added section, added item)","expected", expectedValue, "got", s)
+	}
+	// AddItem to a existing section
+	if b = myIni.AddItem("rename section", "added item", "add value") ; !b {
+		t.Error("For", "AddItem(rename section, added item, add value)","expected", true, "got", b)
+	}
+	// AddItem to a non existing section
+	if b = myIni.AddItem("another new section", "added item", "added value") ; !b {
+		t.Error("For", "AddItem(another new section, added item, another value)","expected", true, "got", b)
 	}
 
-	// SetOrCreate
-	myini.SetOrCreate("new section", "new item", "new value")
-	result, _ 	= myini.Get("new section","new item")
-	if result != "new value" {
-		t.Error("For", "Get(new section,new item)","expected", "new value","got", result)
+	// SetOrCreate Create a new section with new values
+	myIni.SetOrCreate("new section", "new item", "new value")
+	expectedValue = "new value"
+	if s, _ = myIni.Get("new section","new item") ; s != expectedValue {
+		t.Error("For", "Get(new section,new item)","expected", expectedValue,"got", s)
 	}
 
-	// SetOrCreate
-	myini.SetOrCreate("new section", "new item", "another value")
-	result, _ 	= myini.Get("new section","new item")
-	if result != "another value" {
-		t.Error("For", "Get(new section,new item)","expected", "another value","got", result)
+	// SetOrCreate Set an existing value
+	myIni.SetOrCreate("new section", "new item", "another value")
+	expectedValue = "another value"
+	if s, _ = myIni.Get("new section","new item") ; s != expectedValue {
+		t.Error("For", "Get(new section,new item)","expected", expectedValue,"got", s)
 	}
+
 }
